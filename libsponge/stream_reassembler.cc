1e6d02fe5327c2a375e1796d2098e24fef18074c
Megan <meganlee@postech.ac.kr>
2024-03-10 04:57:53 -0700

    initial push_substring

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,24 +1,53 @@
 #include "stream_reassembler.hh"
 
-// Dummy implementation of a stream reassembler.
-
-// For Lab 1, please replace with a real implementation that passes the
-// automated checks run by `make check_lab1`.
-
-// You will need to add private members to the class declaration in `stream_reassembler.hh`
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : 
+    unassembledBase(0),
+    unassembledSize(0),
+    endOfFile(0),
+    dataBuffer(capacity, '\0'),
+    bufferBitmap(capacity, false),
+    _output(capacity), 
+    _capacity(capacity) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    // if the provided data is empty or the index is greater than the capacity, ignore it.
+    if (data.empty() || index >= _capacity) {
+        return;
+    }
+
+    size_t dataStart = index > unassembledBase ? index - unassembledBase : 0; // offset in data
+    size_t dataEnd = dataStart + min(data.size(), _capacity - index); // end index in data
+
+    // update the buffer with the data.
+    for (size_t i = dataStart; i < dataEnd; ++i) {
+        size_t bufferIndex = index + i;
+        
+        // if the byte is not already assembled, update the buffer and set the corresponding bitmap.
+        if (!bufferBitmap[bufferIndex]) {
+            dataBuffer[bufferIndex] = data[i];
+            bufferBitmap[bufferIndex] = true;
+            unassembledSize++;
+        }
+    }
+
+    // update the endOfFile marker if the provided substring contains an EOF marker.
+    if (eof) {
+        endOfFile = index + dataEnd - dataStart;
+    }
+
+    // Cceck if there are contiguous bytes at the beginning of the buffer, write them to output.
+    while (bufferBitmap[unassembledBase]) {
+        char currentChar = dataBuffer[unassembledBase];
+        _output.write(&currentChar); 
+        bufferBitmap[unassembledBase] = false;
+        unassembledBase++;
+        unassembledSize--;
+    }
 }
 
 size_t StreamReassembler::unassembled_bytes() const { return {}; }
